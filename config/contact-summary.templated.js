be7aa42609f389fa4009d7e0ab97438bbcc222be
gmm2204 <gmm2204@cumc.columbia.edu>
2023-11-13 18:30:13 +0300

    Secundus big update

--- config/contact-summary.templated.js
+++ config/contact-summary.templated.js
@@ -1,21 +1,72 @@
+const thisLineage = lineage;
+
 module.exports = {
   fields: [
+    {
+      label: 'contact.profile.outbreak_description',
+      value: contact.description,
+      width: 12,
+      appliesToType: ['outbreak']
+    },
+    {
+      label: 'contact.profile.outbreak_date',
+      value: contact.date,
+      width: 4,
+      appliesToType: ['outbreak']
+    },
+    {
+      label: 'contact.profile.outbreak_status',
+      value: contact.status,
+      width: 4,
+      appliesToType: ['outbreak']
+    },
+    {
+      label: 'contact.profile.county',
+      value: contact.county,
+      width: 4,
+      appliesToType: ['outbreak']
+    },
+    {
+      label: 'contact.profile.sub_county',
+      value: contact.subcounty,
+      width: 4,
+      appliesToType: ['outbreak']
+    },
+    {
+      label: 'contact.profile.ward',
+      value: contact.ward,
+      width: 4,
+      appliesToType: ['outbreak']
+    },
     {
       label: 'contact.profile.role_label',
       value: contact.role_label,
       width: 6,
-      appliesToType: ['person', 'person_contact']
+      appliesToType: ['person']
+    },
+    {
+      label: 'contact.profile.outbreak_location',
+      value: thisLineage,
+      filter: 'lineage',
+      appliesToType: ['case']
+    },
+    {
+      label: 'contact.profile.case',
+      value: thisLineage,
+      filter: 'lineage',
+      appliesToType: ['case_contact']
     },
     {
       label: 'contact.profile.email',
       value: contact.email,
       width: 6,
-      appliesToType: ['person', 'person_contact']
-    }, {
+      appliesToType: ['person', 'case', 'case_contact']
+    },
+    {
       label: 'contact.profile.phone',
       value: contact.phone,
       width: 6,
-      appliesToType: ['person', 'person_contact']
+      appliesToType: ['person', 'case', 'case_contact']
     }
   ],
   cards: [],
--- config/tasks.js
+++ config/tasks.js
@@ -1,25 +1,68 @@
 module.exports = [
-    // {
-    //     name: 'contact_listing',
-    //     title: 'task.icon-user-add-outline-blue',
-    //     icon: 'icon-user-add-outline-blue',
-    //     appliesTo: 'reports',
-    //     appliesToType: ['case_investigation'],
-    //     actions: [{ form: 'case_contact_listing' }],
-    //     events: [{
-    //         start: 14,
-    //         days: 14,
-    //         end: 0,
-    //     }],
-    //     resolvedIf: function (contact, report, event, dueDate) {
-    //         return Utils.isFormSubmittedInWindow(
-    //             contact.reports,
-    //             'case_contact_listing',
-    //             Utils.addDate(dueDate, -event.start).getTime(),
-    //             Utils.addDate(dueDate, event.end + 1).getTime()
-    //         );
-    //     }
-    // }
+    {
+        name: 'case_investigation',
+        title: 'task.title.case_investigation',
+        icon: 'icon-investigation-outline-gray',
+        appliesTo: 'contacts',
+        appliesToType: ['case'],
+        actions: [
+            {
+                form: 'case_investigation',
+                modifyContent: function (content, contact, report, event) {
+                    contact;
+                    report;
+                    event;
+                }
+            }
+        ],
+        events: [{
+            days: 7, //7 days until due
+            start: 7, //Show for 7 days before due day
+            end: 1, //Show for 1 day after due
+        }],
+        resolvedIf: function (contact, report, event, dueDate) {
+            return Utils.isFormSubmittedInWindow(
+                contact.reports,
+                'case_investigation',
+                Utils.addDate(dueDate, -event.start).getTime(),
+                Utils.addDate(dueDate, event.end + 1).getTime()
+            );
+        }
+    },
+    {
+        name: 'case_management',
+        title: 'task.title.case_management',
+        icon: 'icon-management-outline-blue',
+        appliesTo: 'contacts',
+        appliesToType: ['case'],
+        appliesIf: function (c) {
+            return Utils.getMostRecentReport(c.reports, 'case_investigation');
+        },
+        actions: [
+            {
+                form: 'case_management',
+                modifyContent: function (content, contact, report, event) {
+                    content.outbreak_code = JSON.stringify();
+                    contact;
+                    report;
+                    event;
+                }
+            }
+        ],
+        events: [{
+            days: 7, //7 days until due
+            start: 7, //Show for 7 days before due day
+            end: 1, //Show for 1 day after due
+        }],
+        resolvedIf: function (contact, report, event, dueDate) {
+            return Utils.isFormSubmittedInWindow(
+                contact.reports,
+                'case_management',
+                Utils.addDate(dueDate, -event.start).getTime(),
+                Utils.addDate(dueDate, event.end + 1).getTime()
+            );
+        }
+    }
     // {
     //     name: 'task_cholera_lab_report',
     //     title: 'task.lab_form_cholera',
